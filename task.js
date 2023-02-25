c99806292c6f7402670cff8f89e9944e9260ecd7
FarhanHamza <farhan.gmdc@gmail.com>
2023-02-25 15:02:44 +0500

    npm i ng-zorro-antd

---
+++ task.js
@@ -0,0 +1,13 @@
+----------------
+   Ng - Zorro - antd
+----------------
+   npm i ng - zorro - antd
+
+
+Add in Style array:
+"styles": [
+   "node_modules/ng-zorro-antd/src/ng-zorro-antd.min.css",
+   "node_modules/ng-zorro-antd/resizable/style/index.min.css",
+   "node_modules/ng-zorro-antd/code-editor/style/index.min.css",
+   "node_modules/ng-zorro-antd/graph/style/index.min.css"
+]
